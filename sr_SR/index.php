bd84e88061dc9b10610fadd8a1abaf50408b146d
OpenSourceClassifieds <github@opensourceclassifieds.com>
2012-03-06 21:54:03 +0100

    Initial commit

---
+++ sr_SR/index.php
@@ -0,0 +1,33 @@
+<?php
+    /*
+     *      OpenSourceClassifieds ‚ software for creating and publishing online classified
+     *                           advertising platforms
+     *
+     *                        Copyright (C) 2010 OpenSourceClassifieds
+     *
+     *       This program is free software: you can redistribute it and/or
+     *     modify it under the terms of the GNU Affero General Public License
+     *     as published by the Free Software Foundation, either version 3 of
+     *            the License, or (at your option) any later version.
+     *
+     *     This program is distributed in the hope that it will be useful, but
+     *         WITHOUT ANY WARRANTY; without even the implied warranty of
+     *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *             GNU Affero General Public License for more details.
+     *
+     *      You should have received a copy of the GNU Affero General Public
+     * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+     */
+function locale_sr_SR_info() {
+	return array(
+		'name' => 'Serbian'
+		,'short_name' => 'Serbian'
+		,'description' => 'Serbian translation'
+		,'version' => '2.0'
+		,'author_name' => 'Mile'
+		,'author_url' => 'http://www.opensourceclassifieds.org/'
+		,'currency_format' => '%s %.02f'
+		,'date_format' => 'd/m/Y'
+		,'stop_words' => ''
+	);
+}
